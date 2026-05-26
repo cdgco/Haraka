46d528dd5e6386807b18b20604487faa7527019d
Carter Roeser <carter@cdgtech.one>
2026-05-26 10:06:48 -0700

    Update server.js

--- test/server.js
+++ test/server.js
@@ -294,7 +294,7 @@ describe('server', () => {
             const restoreHaproxyConfig = useHaproxyFixture(this.server, 'haproxy_allowed')
             this.server.cfg.main.smtps_port = 0
 
-            const server = await this.server.get_smtp_server(endpoint('127.0.0.1:0'), 30)
+            const server = await this.server.get_smtp_server(endpoint('127.0.0.1:0'), 40)
             const tlsErrors = []
             let raw
             let client
